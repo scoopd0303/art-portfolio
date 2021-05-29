e278b60a21f450f25fed0401025fd19cb93ab59a
Scott McDonald <scottmcdonald@Scotts-MacBook-Air.local>
2021-05-29 11:52:29 -0500

    added spring project folders

---
+++ src/main/java/com/trashdoodles/artportfolio/ArtPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.trashdoodles.artportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trashdoodles/artportfolio/ArtPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trashdoodles.artportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
