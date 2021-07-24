3685d98449aa113bb5ff4b4f3480ad115cd3224c
Scott McDonald <scottmcdonald@Scotts-MacBook-Air.local>
2021-07-24 16:01:34 -0500

    implemented next and previous image capability to lightbox

--- src/main/resources/static/script/index.js
+++ src/main/resources/static/script/index.js
@@ -1,33 +1,66 @@
-// create lightbox element with id of lightbox
-const lightbox = document.createElement('div')
-lightbox.id = 'lightbox'
-document.body.appendChild(lightbox)
-
-const images = document.querySelectorAll('.art-image')
-images.forEach(image => {
-  image.addEventListener('click', e => {
-    lightbox.classList.add('active')
-
-    // find the closest img to the grid-box-mask element
-//    let closestImage = image.closest('.doodle-box').lastElementChild.src;
-//    console.log(closestImage);
-
-    // create new img element with the id of lightbox image and src of lightbox-image
-    const img = document.createElement('img')
-    img.id = 'lightbox-image'
-    img.src = image.src
-
-    // only display one image at a time
-    while (lightbox.firstChild) {
-      lightbox.removeChild(lightbox.firstChild)
+function getPreviousImageElement(img) {
+    if(img.parentElement.parentElement.previousElementSibling != null) {
+        return img.parentElement.parentElement.previousElementSibling.firstElementChild.firstElementChild;
     }
-    // add image to lightbox
-    lightbox.appendChild(img)
+    else {return img;}
+}
 
-  })
-})
+function getNextImageElement(img) {
+    if(img.parentElement.parentElement.nextElementSibling != null) {
+        return img.parentElement.parentElement.nextElementSibling.firstElementChild.firstElementChild;
+    }
+    else {return img;}
+}
 
-lightbox.addEventListener('click', e => {
-  if (e.target !== e.currentTarget) return
-  lightbox.classList.remove('active')
-})
+window.addEventListener("load", function() {
+    // create lightbox element with id of lightbox
+    const lightbox = document.createElement('div')
+    lightbox.id = 'lightbox'
+    document.body.appendChild(lightbox)
+
+    const images = document.querySelectorAll('.gallery-item')
+    images.forEach(image => {
+      image.addEventListener('click', e => {
+        lightbox.classList.add('active')
+
+        let currentImageElement = image.firstElementChild;
+        let previousImageElement;
+        let nextImageElement;
+
+        function setImage(imgElement) {
+            previousImageElement = getPreviousImageElement(imgElement);
+            nextImageElement = getNextImageElement(imgElement);
+            return imgElement.src;
+        }
+
+        // create new img element with the id of lightbox-image and src of image
+        let img = document.createElement('img')
+        img.id = 'lightbox-image'
+        img.src = setImage(currentImageElement);
+
+        // only display one image at a time
+        while (lightbox.firstChild) {
+          lightbox.removeChild(lightbox.firstChild)
+        }
+        // add image to lightbox
+        lightbox.appendChild(img)
+
+        document.onkeydown = (e) => {
+            switch(e.keyCode) {
+                case 37:
+                    img.src = setImage(previousImageElement);
+                    break;
+                case 39:
+                    img.src = setImage(nextImageElement);
+                    break;
+            }
+        }
+
+      })
+    })
+
+    lightbox.addEventListener('click', e => {
+      if (e.target !== e.currentTarget) return
+      lightbox.classList.remove('active')
+    })
+});
